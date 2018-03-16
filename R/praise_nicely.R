df2eb1484b98bc4e41c9e39f68eba2873f9b0aa0
DanDeVilliers <djdevilliers@gmail.com>
2018-03-16 13:43:00 +0200

    - checked package

--- R/praise_nicely.R
+++ R/praise_nicely.R
@@ -6,7 +6,7 @@
 #' @return a praise (character)
 #' @export
 #'
-#' @examples
+#' @examples praise_nicely("everyone")
 praise_nicely <- function(name){
   praise::praise(template = paste0(name, " is ${adjective}")) %>%
     toupper()
